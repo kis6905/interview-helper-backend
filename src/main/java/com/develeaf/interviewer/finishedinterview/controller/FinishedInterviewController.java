e4c83aef6b651c0c2b03274777df2f8ba32e4e21
kis6905 <kis6905@gmail.com>
2020-07-21 21:43:22 +0900

    add

---
+++ src/main/java/com/develeaf/interviewer/finishedinterview/controller/FinishedInterviewController.java
@@ -0,0 +1,10 @@
+package com.develeaf.interviewer.finishedinterview.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/finishedinterview")
+public class FinishedInterviewController {
+	
+	
+	
+}
