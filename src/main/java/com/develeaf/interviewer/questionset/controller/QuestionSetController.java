339431273a04bfa2985fd981a6999ef93743d733
kis6905 <kis6905@gmail.com>
2020-07-22 21:55:42 +0900

    add controller

---
+++ src/main/java/com/develeaf/interviewer/questionset/controller/QuestionSetController.java
@@ -0,0 +1,8 @@
+package com.develeaf.interviewer.questionset.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/question/set")
+public class QuestionSetController {
+
+}
