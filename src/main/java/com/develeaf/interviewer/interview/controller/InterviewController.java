4a2756c1a99bb2c935825203451903472ceb17ea
kis6905 <kis6905@gmail.com>
2020-07-23 21:56:54 +0900

    add interviewController

---
+++ src/main/java/com/develeaf/interviewer/interview/controller/InterviewController.java
@@ -0,0 +1,8 @@
+package com.develeaf.interviewer.interview.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/interview")
+public class InterviewController {
+
+}
