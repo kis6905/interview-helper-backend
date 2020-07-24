a7dc1d6b09ab24fffec20bc1a8593ad3a966fb1b
kis6905 <kis6905@gmail.com>
2020-07-24 23:14:31 +0900

    add

---
+++ src/main/java/com/develeaf/interviewer/common/dto/CommonDto.java
@@ -0,0 +1,11 @@
+package com.develeaf.interviewer.common.dto;
+
+import lombok.Data;
+
+@Data
+public class CommonDto {
+	
+	private String registeredDate;
+	private String modifiedDate;
+	
+}
--- src/main/java/com/develeaf/interviewer/finishedinterview/controller/FinishedInterviewController.java
+++ src/main/java/com/develeaf/interviewer/finishedinterview/controller/FinishedInterviewController.java
@@ -1,10 +1,18 @@
 package com.develeaf.interviewer.finishedinterview.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.develeaf.interviewer.common.dto.CommonDto;
 
 @RestController("/finishedinterview")
 public class FinishedInterviewController {
 	
-	
+	@GetMapping("/list")
+	public ResponseEntity<CommonDto> getFinishedInterviewList() {
+		
+		return ResponseEntity.ok(new CommonDto());
+	}
 	
 }
