c5d0726d533484f1d96ede88efda0ba70d0daa29
kis6905 <kis6905@gmail.com>
2020-07-25 15:57:59 +0900

    add

--- src/main/java/com/develeaf/interviewer/questionset/controller/QuestionSetController.java
+++ src/main/java/com/develeaf/interviewer/questionset/controller/QuestionSetController.java
@@ -1,8 +1,18 @@
 package com.develeaf.interviewer.questionset.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.develeaf.interviewer.common.dto.CommonDto;
 
 @RestController("/question/set")
 public class QuestionSetController {
-
+	
+	@GetMapping("/list")
+	public ResponseEntity<CommonDto> getQuestionSetList() {
+		
+		return ResponseEntity.ok(new CommonDto());
+	}
+	
 }
