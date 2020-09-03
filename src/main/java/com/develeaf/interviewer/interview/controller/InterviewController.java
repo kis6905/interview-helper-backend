266f9cddd37d3053dede531ebffadde7c17b889a
kis6905 <kis6905@gmail.com>
2020-09-03 23:02:31 +0900

    interview dev...

--- src/main/java/com/develeaf/interviewer/interview/controller/InterviewController.java
+++ src/main/java/com/develeaf/interviewer/interview/controller/InterviewController.java
@@ -1,8 +1,18 @@
 package com.develeaf.interviewer.interview.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.develeaf.interviewer.common.view.Response;
 
 @RestController("/interview")
 public class InterviewController {
-
+	
+	@GetMapping("/list/")
+	public Response<?> getQuestionSetList(@PathVariable(name = "companySeq", required = true) Long companySeq) {
+		// TODO: 해야함
+		return Response.createSuccess();
+	}
+	
 }
